891a05791a6e919aa89591cebdaa1f2c60100247
Zilnyk Andrii <azilnyk-c@pivotalcommware.com>
2023-06-06 01:06:22 +0300

    added basic navigation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <CategoriesScreen />
       <StatusBar style='light' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+          <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
