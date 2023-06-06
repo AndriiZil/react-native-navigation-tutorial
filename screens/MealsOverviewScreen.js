7f6ac10c6b132b19cf2db1897d49a6ba63354929
Zilnyk Andrii <azilnyk-c@pivotalcommware.com>
2023-06-06 21:17:30 +0300

    added mealItem element

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,34 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const { categoryId } = route.params;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem({ item }) {
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    }
+
+    return <MealItem {...mealItemProps} />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={({ id }) => id}
+        renderItem={renderMealItem}
+      />
     </View>
   )
 }
